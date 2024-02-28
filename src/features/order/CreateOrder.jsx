ea5cdc19ac4985649f260ffefe699f3015b00caa
Heber Solano <hebersolano1@gmail.com>
2024-02-27 19:45:36 -0600

    tailwind styles 0.3

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -41,31 +41,39 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let&apos;s go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let&apos;s go!
+      </h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
-        </div>
-
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2  sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <div className="grow">
+            <input className="input" type="text" name="customer" required />
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div className="mb-5 flex flex-col gap-2  sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input" type="tel" name="phone" required />
+            {formErrors?.phone && (
+              <p className="mt-2 pl-6  text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="mb-5 flex flex-col gap-2  sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
             <input className="input" type="text" name="address" required />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             type="checkbox"
             name="priority"
@@ -74,7 +82,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
 function Button({ handleClick, to, disable, type = "primary", children }) {
   const tailwind = {
-    base: "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 ",
+    base: "inline-block rounded-full text-sm bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 ",
     get primary() {
-      return this.base + " px-4 py-3 md:py-4";
+      return twMerge(this.base, " px-4 py-3 md:py-4");
     },
     get small() {
-      return this.base + " py-2 px-4 md:px-5 md:py-2.5 text-xs";
+      return twMerge(this.base, " py-2 px-4 md:px-5 md:py-2.5 text-xs");
     },
     get secondary() {
-      return (
-        this.base +
-        "px-4 py-2.5 md:py-4 bg-transparent border-2 border-stone-400 text-stone-400 md:py-3.5 hover:bg-stone-200 hover:text-stone-600 hover:border-stone-600"
+      return twMerge(
+        this.base,
+        "px-4 py-2.5 bg-transparent border-2 border-stone-400 text-stone-400 md:py-3.5 hover:bg-stone-200 hover:text-stone-600 hover:border-stone-600",
       );
     },
   };
